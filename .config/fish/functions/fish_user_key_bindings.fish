28c26142b00e9f6bb063060829115bb55210c890
Øystein Krog <oystein.krog@gmail.com>
2019-04-11 13:51:27 +0200

    fish/keybindings: fix vi/custom keybindings, wip

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,1 +1,15 @@
+fish_vi_keybindings
+
+# vi mode colemak/custom overrides
+#
+# Default (command) mode
+bind -s --preset -M default n backward-char
+bind -s --preset -M default i forward-char
+bind -s --preset -m insert l force-repaint
+bind -s --preset -m insert L beginning-of-line force-repaint
+
+bind -s --preset \cr fzy_select_history
+
+bind -s --preset u up-or-search
+bind -s --preset e down-or-search
 bind \cr re_search
