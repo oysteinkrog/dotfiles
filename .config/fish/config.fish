02d8c5fa22de875b567f73d9acd28e2a0fc80f98
Øystein Krog <oystein.krog@gmail.com>
2019-04-13 13:04:16 +0200

    fish: lots of fixes

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+
+# disable default vi mode prompt prefix
+function fish_mode_prompt
+end
+
+
--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,15 +1,26 @@
-fish_vi_keybindings
+function fish_user_key_bindings --description "User key bindings for fish"
+    fish_vi_key_bindings
 
-# vi mode colemak/custom overrides
-#
-# Default (command) mode
-bind -s --preset -M default n backward-char
-bind -s --preset -M default i forward-char
-bind -s --preset -m insert l force-repaint
-bind -s --preset -m insert L beginning-of-line force-repaint
+    # vi mode colemak/custom overrides
+    #
+    # Default (command) mode
+    bind -s --preset -M default n backward-char
+    bind -s --preset -M default i forward-char
+    bind -s --preset -m insert l force-repaint
+    bind -s --preset -m insert L beginning-of-line force-repaint
 
-bind -s --preset \cr fzy_select_history
+    bind -s --preset \cr fzy_select_history
 
-bind -s --preset u up-or-search
-bind -s --preset e down-or-search
-bind \cr re_search
+    bind -s --preset u up-or-search
+    bind -s --preset e down-or-search
+
+    #hybrid_bindings
+    #fish_vi_key_bindings
+
+    #bind \cr re_search
+    bind \cr 'fzy_select_history (commandline -b)'
+    bind -M insert \cr 'fzy_select_history (commandline -b)'
+
+    bind \cf 'fzy_select_directory'
+end
+
